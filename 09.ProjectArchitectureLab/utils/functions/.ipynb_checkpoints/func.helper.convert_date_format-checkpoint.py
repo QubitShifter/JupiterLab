00bc6c49f90ffe1190285caf8e55b1150f348abe
JoroPetroff <joro.petroff@gmail.com>
2025-08-14 12:57:23 +0300

    adding config file and functions

--- 09.ProjectArchitectureLab/utils/functions/.ipynb_checkpoints/func.helper.convert_date_format-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def conver_date_format(df: pd.DataFrame, date_column: str = 'Date') -> pd.DataFrame:
-    df = df.copy()
-    df[date_column] = pd.to_datetime(df[date_column], error = 'coerce')
-    return df
